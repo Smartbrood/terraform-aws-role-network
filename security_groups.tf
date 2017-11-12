69c8ee00fa485abd148ab97f92afe1d3cbe96d23
Andrey N. Petrov <andreynpetrov@gmail.com>
2017-11-12 22:37:13 +0300

    intermediate auto

--- security_groups.tf
+++ security_groups.tf
@@ -1,4 +1,4 @@
-module "security_group_ssh" {
+module "security_group" {
   source = "Smartbrood/security-group/aws"
   name        = "${var.security_group_name}"
   description = "created by terraform role-network module"
@@ -7,10 +7,10 @@ module "security_group_ssh" {
   ingress_rules_from_any = "${var.ingress_rules_from_any}"
   egress_rules_to_any    = "${var.egress_rules_to_any}"
 
-  ingress_rules_from_any = "${var.ingress_rules}"
+  ingress_rules          = "${var.ingress_rules}"
   ingress_cidr_blocks    = "${var.ingress_cidr_blocks}"
 
-  egress_rules_to_any    = "${var.egress_rules}"
+  egress_rules           = "${var.egress_rules}"
   egress_cidr_blocks     = "${var.egress_cidr_blocks}"
 
   tags               = "${var.tags}"
