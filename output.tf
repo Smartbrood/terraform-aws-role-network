94e6957cb55e491b68c16ecec4b4fb73c6142d1e
Andrey N. Petrov <andreynpetrov@gmail.com>
2017-11-14 16:50:29 +0300

    auto intermediate

--- output.tf
+++ output.tf
@@ -8,9 +8,14 @@ output "security_group_id" {
   value       = "${module.security_group.security_group_id}"
 }
 
-output "customer_gateway_configuration" {
+output "primary_gateway_configuration" {
   description = "The configuration information for the VPN connection's customer gateway"
-  value = "${module.vpn.customer_gateway_configuration}"
+  value = "${module.vpn.primary_gateway_configuration}"
+}
+
+output "secondary_gateway_configuration" {
+  description = "The configuration information for the VPN connection's customer gateway"
+  value = "${module.vpn.secondary_gateway_configuration}"
 }
 
 output "vpc_id" {
